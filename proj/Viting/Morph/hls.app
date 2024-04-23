063b74463e90b808ae33d3366b5e52864acea1ff
Daniel <daniel.stebbings.2021@uni.strath.ac.uk>
2024-04-23 17:37:56 +0100

    hls update

--- proj/Viting/Morph/hls.app
+++ proj/Viting/Morph/hls.app
@@ -5,6 +5,7 @@
         <SimFlow name="csim" csimMode="0" lastCsimMode="0"/>
     </Simulation>
     <files xmlns="">
+        <file name="../../window_tb.cpp" sc="0" tb="1" cflags=" -Wno-unknown-pragmas" csimflags=" -Wno-unknown-pragmas" blackbox="false"/>
         <file name="morph.h" sc="0" tb="false" cflags="" csimflags="" blackbox="false"/>
         <file name="main.cpp" sc="0" tb="false" cflags="" csimflags="" blackbox="false"/>
     </files>
--- proj/Viting/main.cpp
+++ proj/Viting/main.cpp
@@ -1,60 +1,70 @@
 //INCLUDES
 #include <algorithm>
+#include <array>
+#include "ap_int.h"
 
 //define constants
-#define WINDOW_SIZE 3
-#define WIDTH 162
-#define HEIGHT 122
+#define WINDOW_SIZE    3
+#define WIDTH  		 160
+#define HEIGHT 		 120
 
-char line_buffer[WINDOW_SIZE][WIDTH];
+typedef std::array<ap_uint<1>,WIDTH*WINDOW_SIZE> buffer;
 
 
-void window(char p, //inputs
-		char w00, char w01, char w02, //window outputs
-		char w10, char w11, char w12,
-		char w20, char w21, char w22)
+
+
+void window(ap_uint<1> p, //inputs
+		char* w00, char* w01, char* w02, //window outputs
+		char* w10, char* w11, char* w12,
+		char* w20, char* w21, char* w22)
 {
-#pragma HLS INTERFACE ap_none port=p,w00,w01,w02,w10,w11,w12,w20,w21,w22
+#pragma HLS INTERFACE ap_hs      port=w00,w01,w02,w10,w11,w12,w20,w21,w22 //window out
+#pragma HLS INTERFACE ap_hs 	 port=p //pixel in
 #pragma HLS INTERFACE ap_ctrl_hs port=return
-#pragma HLS PIPELINE  II=1 //1 input/clock
+
+	buffer line_buffer;
 
 	//initial line buffer
 	BUF_INIT:for(int i = 0; i<= WIDTH*WINDOW_SIZE; i++) {
-		int row = i/WIDTH;
-		int col = i%WIDTH;
-		line_buffer[row][col] = p;
+#pragma HLS PIPELINE  II=1 //1 input/clock
+		line_buffer[i] = char(p);
 	}
 
 	//iterate through image row by row
-	ROW:for(int imrow= 0; imrow<=HEIGHT-WINDOW_SIZE;imrow++){
-#pragma HLS unroll off=true //disable unrolling
-		COL:for(int col = 0; col<=WIDTH-(WINDOW_SIZE-1);col++) {
-#pragma HLS unroll
-			w00 = line_buffer[0][col];
-			w01 = line_buffer[0][col+1];
-			w02 = line_buffer[0][col+2];
+	ROW:for(int imrow= WINDOW_SIZE-1; imrow<=HEIGHT;imrow++){
+#pragma HLS unroll factor=1 //disable unrolling
+		COL:for(int col = WINDOW_SIZE-1; col<=WIDTH;col++) {
+#pragma HLS PIPELINE  II=1 //1 input/clock
 
-			w10 = line_buffer[1][col];
-			w11 = line_buffer[1][col+1];
-			w12 = line_buffer[1][col+2];
+			*w00 = line_buffer[0*WIDTH + col];
+			*w01 = line_buffer[0*WIDTH + col+1];
+			*w02 = line_buffer[0*WIDTH + col+2];
 
-			w20 = line_buffer[2][col];
-			w21 = line_buffer[2][col+1];
-			w22 = line_buffer[2][col+2];
+			*w10 = line_buffer[0*WIDTH + col];
+			*w11 = line_buffer[0*WIDTH + col+1];
+			*w12 = line_buffer[0*WIDTH + col+2];
+
+			*w20 = line_buffer[2][col-2];
+			*w21 = line_buffer[2][col-1];
+			*w22 = line_buffer[2][col];
+
 		}
-
+		buffer temp_buffer;
 		//Finished line buffer, discard first line. Move remaining lines up, read in next line
-		std::copy(&line_buffer[1][0], &line_buffer[2][2],&line_buffer[0][0]); //start, end , destination
+		std::copy(line_buffer.begin()+WIDTH, line_buffer.end(),temp_buffer.begin()); //start, end , destination
+
+		//temp_buffer[0] = line_buffer[1];
+		//temp_buffer[1] = line_buffer[2];
+
 
 		//Read in next line
-		BUFF_UPDATE: for (int i=0; i<=WIDTH; i++) {
-			line_buffer[WINDOW_SIZE-1][i] = p;
+		BUFF_UPDATE: for (int i=0; i<WIDTH; i++) {
+			temp_buffer[2][i] = p;
 		}
 
+		line_buffer = temp_buffer;
 	}
-
 	//image finished
 	return;
-
 }
 
